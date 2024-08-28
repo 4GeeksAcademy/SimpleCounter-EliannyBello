93ee966881a21325a1bb3618e72dcb776478fabd
EliannyBello <elibellomanzo@gmail.com>
2024-08-28 21:31:36 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const SimpleCounter = () => {
+    const [counter, setCounter] = useState(0);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter(prevCounter => prevCounter + 1);
+        }, 1000);
 
-export default Home;
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const four = Math.floor(counter / 1000) % 10;
+    const three = Math.floor(counter / 100) % 10;
+    const two = Math.floor(counter / 10) % 10;
+    const one = Math.floor(counter % 10);
+
+    return (
+        <div className="container d-flex justify-content-around align-items-center">
+            <div className='col-md-4 d-flex justify-content-center'>
+            <h6>Timer</h6>
+                <i className="far fa-clock fa-2x"></i>
+            </div>
+            <div className='col-md-2 d-flex justify-content-center'>
+                <div>{four}</div>
+            </div>
+            <div className='col-md-2 d-flex justify-content-center'>
+                <div>{three}</div>
+            </div>
+            <div className='col-md-2 d-flex justify-content-center'>
+                <div>{two}</div>
+            </div>
+            <div className='col-md-2 d-flex justify-content-center'>
+                <div>{one}</div>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default SimpleCounter;
